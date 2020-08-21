7b079538c02fe1908b4d03a3f15c4a131dfecb6f
WanderingSoulsPreson <41480977+WanderROS@users.noreply.github.com>
2020-08-22 07:41:47 +0800

    Update HelloController.java

--- src/main/java/com/jenkins/web/HelloController.java
+++ src/main/java/com/jenkins/web/HelloController.java
@@ -10,7 +10,7 @@ public class HelloController {
 
     @GetMapping("/")
     public String helloWorld(){
-        return "Hello World! Jenkins!!!";
+        return "Hello World! Jenkins!!!----";
     }
 
 }
